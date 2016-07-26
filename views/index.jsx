301a8eb6b010ee5662791d718dce59ed4c1d378c
Control Zeta <francisco.arroyo@controlzeta.com.mx>
2016-07-26 17:12:32 -0500

    Exercise 5 of 11
    Learn React.js
    ────────────────
    STATE
    Exercise 5 of 11

--- views/index.jsx
+++ views/index.jsx
@@ -30,10 +30,25 @@
     }
 
     class Todo extends React.Component {
-      // Write code here
+      constructor(props){
+        super(props);
+        this.state = {
+            checked : false
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+      }
+
+
+        handleChange(event) {
+            this.setState({checked: event.target.value});
+        }
       render() {
             return (
                 <tr>
+                    <td style={{border: "1px solid black"}}>
+                        <input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/>
+                    </td>
                     <td style={{border: "1px solid black"}} >{this.props.title}</td>
                     <td style={{border: "1px solid black"}} >{this.props.children}</td>
                 </tr>
